8f50d29beb7042dc3cad7c73c7f5be7a5a7e082f
LocPhan <locphanwork@gmail.com>
2022-10-25 14:29:59 +0700

    db

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   user: 'postgres',
-  password: 'qwe',
-  host: 'localhost',
-  port: 5432,
-  database: 'restaurant',
+  password: 'jxanxaO6G8CFMziYbA49',
+  host: 'containers-us-west-104.railway.app',
+  port: 6928,
+  database: 'railway',
 });
 
 module.exports = pool;
