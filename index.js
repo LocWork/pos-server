4d8517631a654fbfe70791f06dacefe9a388881b
LocPhan <locphanwork@gmail.com>
2022-11-07 15:14:05 +0700

    branch deplout

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.use('/search', searchRoute);
 app.use('/transferdetail', transferDetailRoute);
 app.use('/cashierlog', cashierlogRoute);
 
-server.listen(PORT, () => {
+server.listen(`0.0.0.0:${PORT}`, () => {
   console.log('Server running...');
 });
 
