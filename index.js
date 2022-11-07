291517abffbc84e545d109d7ed0696195578872f
LocPhan <locphanwork@gmail.com>
2022-11-07 12:43:37 +0700

    json

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const session = require('express-session');
 const app = express();
-// const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 // socket
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, { cors: { origin: '*' } });
@@ -106,7 +105,7 @@ app.use('/search', searchRoute);
 app.use('/transferdetail', transferDetailRoute);
 app.use('/cashierlog', cashierlogRoute);
 
-server.listen(process.env.PORT, () => {
+server.listen(PORT, () => {
   console.log('Server running...');
 });
 
