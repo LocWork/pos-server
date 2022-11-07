2d22b630dff67d5ed642ab5aa4b0db849905598a
LocPhan <locphanwork@gmail.com>
2022-11-07 13:02:02 +0700

    json

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.use('/search', searchRoute);
 app.use('/transferdetail', transferDetailRoute);
 app.use('/cashierlog', cashierlogRoute);
 
-server.listen(PORT, `0.0.0.0:${PORT}`, () => {
+server.listen(`0.0.0.0`, () => {
   console.log('Server running...');
 });
 
