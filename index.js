7b78ecee6a4d14d4e6e0e68663cb2dc8e21d6574
LocPhan <locphanwork@gmail.com>
2022-11-07 15:36:51 +0700

    change port

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.use('/search', searchRoute);
 app.use('/transferdetail', transferDetailRoute);
 app.use('/cashierlog', cashierlogRoute);
 
-server.listen(PORT, () => {
+server.listen(`0.0.0.0`, () => {
   console.log('Server running...');
 });
 
