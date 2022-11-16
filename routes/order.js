b04f54d9634bd9555e0224130c5936a8d82575d3
LocPhan <locphanwork@gmail.com>
2022-11-16 09:43:25 +0700

    redeploy

--- routes/order.js
+++ routes/order.js
@@ -214,7 +214,7 @@ router.get('/check/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const check = await pool.query(
-      `SELECT C.id AS checkid,C.checkno,C.subtotal,C.totalTax, C.totalamount, (C.creationtime::TIMESTAMP::DATE) as date, date::time AS creationtime , L.id AS locationid, T.id AS tableid
+      `SELECT C.id AS checkid,C.checkno,C.subtotal,C.totalTax, C.totalamount, (C.creationtime::TIMESTAMP::DATE)::time AS creationtime, L.id AS locationid, T.id AS tableid
       FROM "check" AS C
       JOIN "table" AS T
       ON C.tableid = T.id
